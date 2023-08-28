4274230519fd4249f8f632c3f185ea8cc123467b
Samuel Trezitorul <trezitorul@gmail.com>
2023-08-28 17:52:59 -0400

    Initial commit

---
+++ src/qudi/hardware/template_hardware.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['TemplateHardware']
+
+import time
+from PySide2 import QtCore
+
+from qudi.interface.template_interface import TemplateInterface
+from qudi.core.statusvariable import StatusVar
+from qudi.core.configoption import ConfigOption
+from qudi.util.mutex import Mutex
+
+
+class TemplateHardware(TemplateInterface):
+    """ This is a simple template hardware measurement module for qudi.
+    """
+
+    # Declare static parameters that can/must be declared in the qudi configuration
+    _trigger_time = ConfigOption(name='trigger_time', default=0.001, missing='warn')
+
+    # Declare status variables that are saved in the AppStatus upon deactivation of the module and
+    # are initialized to the saved value again upon activation.
+    # _my_status_variable = StatusVar(name='my_status_variable', default=0)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._mutex = Mutex()
+
+    def on_activate(self) -> None:
+        pass
+
+    def on_deactivate(self) -> None:
+        pass
+
+    @property
+    def trigger_time(self) -> float:
+        return self._trigger_time
+
+    @QtCore.Slot()
+    def send_trigger(self) -> None:
+        with self._mutex:
+            time.sleep(self._trigger_time)
---
+++ src/qudi/interface/template_interface.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['TemplateInterface']
+
+from PySide2 import QtCore
+from abc import abstractmethod
+
+from qudi.core.module import Base
+
+
+class TemplateInterface(Base):
+    """ This is a simple template hardware interface for qudi.
+    """
+    @property
+    @abstractmethod
+    def trigger_time(self) -> float:
+        """ Read-only property holding the trigger high duration.
+        """
+        pass
+
+    @abstractmethod
+    def send_trigger(self) -> None:
+        """ Send a single trigger.
+        """
+        pass
